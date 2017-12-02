44cabc804e6053c88c57fb455127600d24de6f8f
Tian Zhao <ztian1994@hotmail.com>
2017-12-01 19:12:55 -0500

    ReadFile function
    ReadFile-0

--- src/TM/Engine.java
+++ src/TM/Engine.java
@@ -1,11 +1,24 @@
 package TM;
 
 import DM.Variable;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Engine {
+  private void readFile(String inputFile) throws IOException{
+    FileInputStream fis = new FileInputStream(inputFile);
+    BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+    String line;
+    while ((line = br.readLine()) != null) {
+      System.out.println(line);
+    }
+    br.close();
+  }
 
-
-  public static void main(String[] args) {
-
+  public static void main(String[] args) throws IOException {
+    Engine taskManager = new Engine();
+    taskManager.readFile("C:\\Users\\ztian\\Downloads\\ADB-project\\sample\\test1.txt");
   }
 }
